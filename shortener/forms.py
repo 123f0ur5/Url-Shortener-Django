0cd15fe15304cf92b9a0ce6367032dfdd6a34d4b
123f0ur5 <nagazaky96@gmail.com>
2022-09-10 21:15:05 -0300

    some changes

--- shortener/forms.py
+++ shortener/forms.py
@@ -1,6 +1,18 @@
 from django import forms
 from .models import Url
 
-class UrlForm(forms.Form):
-	model = Url
+class UrlForm(forms.ModelForm):
 	original_link	= forms.CharField(label='Your Link', widget=forms.TextInput(attrs={"placeholder" : "Your Link"}))
+	
+	class Meta:
+		model = Url
+		fields = [
+			'original_link',
+		]
+
+	def clean_original_link(self):
+		original_link = self.cleaned_data.get("original_link")
+		if '.' not in original_link:
+			raise forms.ValidationError("Insert a valid Link")
+		else:
+			return original_link
--- shortener/views.py
+++ shortener/views.py
@@ -5,15 +5,14 @@ import string, random
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
-	my_url = UrlForm()
-	if request.method == "POST":
-		my_url = UrlForm(request.POST)
-		if my_url.is_valid():
-			link = request.POST.get('original_link')
-			identify = generate_identifier()
-			Url.objects.create(original_link=link, identifier=identify)
-			return redirect('shorten/'+identify, identify)
-			my_url = UrlForm()
+	my_url = UrlForm(request.POST or None)
+	if my_url.is_valid():
+		link = request.POST.get('original_link')
+		link = fix_link(link)
+		identify = generate_identifier()
+		Url.objects.create(original_link=link, identifier=identify)
+		return redirect('shorten/'+identify, identify)
+		my_url = UrlForm()
 	context = {
 		'my_url' : my_url,
 	}
@@ -38,3 +37,11 @@ def generate_identifier(size = 5, chars=string.ascii_uppercase + string.digits +
 		identify = ''.join(random.choice(chars) for _ in range(size))
 
 	return identify
+
+def fix_link(link):
+	if 'www' not in link:
+		link = 'www.' + link
+	if 'http://' not in link:
+		link = 'http://' + link
+
+	return link
